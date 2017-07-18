2373c741a5d3f394dbcb42d9e2df3850714d8c6c
Robe Costa <A644308@MC0X2NKC.ww930.my-it-solutions.net>
2017-07-18 13:50:59 +0100

    modificados metodos para insertar,editar y eliminar. Simplificado codigo

--- src/sources/GestionarDivisiones.java
+++ src/sources/GestionarDivisiones.java
@@ -22,53 +22,32 @@ public class GestionarDivisiones {
         
     }
     
-    public void guardarDivision(String division){
-        try {
+    public void guardarDivision(String division) throws SQLException{
+
             conn.conectar();
             conn.insertData("division", "null,'" + division + "'" );
             conn.getConection().commit();
             conn.desconectar();
-            ImageIcon icon = new ImageIcon(getClass().getResource("/resources/aceptar.png"));
-            JOptionPane.showMessageDialog(null, "División guardada con éxito", "Información", JOptionPane.QUESTION_MESSAGE, icon);
-        } catch (SQLException ex) {
-            ImageIcon icon = new ImageIcon(getClass().getResource("/resources/warning.png"));
-            JOptionPane.showMessageDialog(null, "Error al guardar los datos\n"+ex.getMessage(), "Error", JOptionPane.QUESTION_MESSAGE, icon);
-        }
     }
     
-    public void editarDivision(String anterior, String nuevo){
-        try{
+    public void editarDivision(String anterior, String nuevo) throws SQLException{
             conn.conectar();
             conn.updateData("division", "DIVISION = '" + nuevo + "'", "DIVISION LIKE '" + anterior+ "'");
             conn.getConection().commit();
             conn.desconectar();
-        }catch(SQLException ex){
-            ImageIcon icon = new ImageIcon(getClass().getResource("/resources/warning.png"));
-            JOptionPane.showMessageDialog(null, "Error al editar los datos\n"+ex.getMessage(), "Error", JOptionPane.QUESTION_MESSAGE, icon);
-        }
     }
     
-    public void eliminarDivision(String division){
-        try{
+    public void eliminarDivision(String division) throws SQLException{
             conn.conectar();
             conn.deleteData("division", "DIVISION like '" + division + "'");
             conn.getConection().commit();
             conn.desconectar();
-        }catch(SQLException ex){
-            ImageIcon icon = new ImageIcon(getClass().getResource("/resources/warning.png"));
-            JOptionPane.showMessageDialog(null, "Error al borrar los datos\n"+ex.getMessage(), "Error", JOptionPane.QUESTION_MESSAGE, icon);
-        }
     }
-    public void vaciarDivision(){
-        try{
+    public void vaciarDivision() throws SQLException{
             conn.conectar();
             conn.deleteTable("division");
             conn.getConection().commit();
             conn.desconectar();
-        }catch(SQLException ex){
-            ImageIcon icon = new ImageIcon(getClass().getResource("/resources/warning.png"));
-            JOptionPane.showMessageDialog(null, "Error al vaciar los datos\n"+ex.getMessage(), "Error", JOptionPane.QUESTION_MESSAGE, icon);
-         }
     }
     
 }
