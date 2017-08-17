5aed642947d1335ef73c2ddf8a15ba40b25bda40
Roberto <rob.antunez@gmail.com>
2017-08-17 21:30:38 +0100

    por terminar metodo insertar dia festivo

--- src/sources/GestionarFestivos.java
+++ src/sources/GestionarFestivos.java
@@ -1,10 +1,21 @@
 package sources;
 
+import connection.Conn;
+import java.sql.SQLException;
+
 /**
  *
  * @author santy
  */
 public class GestionarFestivos {
+    private final Conn conn = new Conn();
     
+        public void guardarDivision(String fecha_inicio) throws SQLException{
+
+            conn.conectar();
+      //      conn.insertData("festivos", "null,'" + division + "'" );
+            conn.getConection().commit();
+            conn.desconectar();
+    }
     
 }
